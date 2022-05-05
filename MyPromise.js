3cd9187e9f94150a4608b6ebc4e81355ff90e4cf
choeqq <k.chola@op.pl>
2022-05-06 00:13:01 +0200

    allSettled method added

--- MyPromise.js
+++ MyPromise.js
@@ -152,6 +152,29 @@ class MyPromise {
       }
     });
   }
+
+  static allSettled(promise) {
+    const results = [];
+    let completedPromises = 0;
+    return new MyPromise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        const promise = promises[i];
+        promise
+          .then((value) => {
+            results[i] = { status: STATE.FULFILLED, value };
+          })
+          .catch((reason) => {
+            results[i] = { status: STATE.REJECTED, reason };
+          })
+          .finally(() => {
+            completedPromises++;
+            if (completedPromises === promises.length) {
+              resolve(results);
+            }
+          });
+      }
+    });
+  }
 }
 
 class UncaughtPromiseError extends Error {
